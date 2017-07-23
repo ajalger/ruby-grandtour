ed4b6d4834658ec37aad431c4d3dfe039992fc0b
Abdullah Alger <abdullahalger@me.com>
2017-07-22 20:11:37 -0700

    added env for mongodb connection

--- example-mongodb/app.rb
+++ example-mongodb/app.rb
@@ -4,7 +4,7 @@ require 'mongo'
 # configure the MongoDB database and set the collection 
 # to "words" which will hold a word and its description 
 configure do 
-    client = Mongo::Client.new('mongodb://<username>:<password>@aws-us-west-2-portal.2.dblayer.com:16043,aws-us-west-2-portal.1.dblayer.com:16043/examples?ssl=true', :ssl => true, :ssl_verify => false)
+    client = Mongo::Client.new(ENV["MONGODB_URI"], :ssl => true, :ssl_verify => false)
     set :collection, client['words']
 end
 
