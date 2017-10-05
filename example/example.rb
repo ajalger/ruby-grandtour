58a45e449a4463b67ac68645bae11896f2a63b55
Abdullah Alger <abdullah.alger@ibm.com>
2017-10-05 15:13:15 -0700

    fixed example to store data in array

--- example/example.rb
+++ example/example.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+words = []
+
 get '/' do 
     # serves the top-level page display
     File.read(File.join('public', 'index.html'))
@@ -9,13 +11,15 @@ get '/words' do
     # queries all words in the collection and returns it as a JSON document to display on the page 
     # content is set to application/json
     content_type :json
+    words.to_json
 end
 
 put '/words' do
+    status 200
     # when the 'Add' button is clicked it will create a new document to send to the database
     # grabs the word and the description from the HTML input boxes
-    new_word = {word: params[:word], definition: params[:definition]}
-    status 200
+    words << {word: params[:word], definition: params[:definition]}
+    redirect '/'
 end
 
 
